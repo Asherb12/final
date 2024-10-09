4967a19b42ec248fc25bc9a9fa019a5309d877fe
Asherb12 <asherheitz@icloud.com>
2024-10-09 13:13:59 -0400

    aa

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var correctq1 = $('#correct1');
+var incorrectq1 = $('#incorrect1');
+var score = 0;
+var totalq = 10;
 
-correctq1.on("click",toggleFunction)
+correctq1.on("click",toggle);
+incorrectq1.on("click",toggle);
 
 
 
 
 
+
+function togglec(){
+    correctq1.toggleClass("correct");
+    score++;
+}
+function togglei(){
+    incorrectq1.toggleClass("incorrect");
+}
